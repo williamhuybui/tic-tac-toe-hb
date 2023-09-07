804710335901bff39407830d314db029168be5be
Huy Bui <williamhuybui@gmail.com>
2023-09-07 18:54:40 -0500

    fix diagonal

--- src/components/Game.js
+++ src/components/Game.js
@@ -2,18 +2,17 @@ import React, { useState, useEffect } from "react";
 import Board from "./Board";
 
 function Game() {
-
   const [n, setN] = useState(6); //n is the number of rows and columns in the board
   const [k, setK] = useState(3); //k is the number of squares in a row to win
-  const [squares, setSquares] = useState(Array(n**2).fill(null));
+  const [squares, setSquares] = useState(Array(n ** 2).fill(null));
   const [xIsNext, setXIsNext] = useState(true);
   const [winner, setWinner] = useState(null);
   const [steps, setSteps] = useState([squares]);
 
   const handleHistoryClick = (steps, index) => {
     setXIsNext(index % 2 === 0);
-    setSquares(steps[index])
-  }
+    setSquares(steps[index]);
+  };
   function History(props) {
     const { steps } = props;
     return steps.map((step, index) => (
@@ -22,8 +21,8 @@ function Game() {
           {index === 0 ? "Go to game start" : `Go to move #${index}`}
         </button>
       </li>
-    ))
-}
+    ));
+  }
 
   //function to check if a player has won.
   //If a player has won, we can display text such as “Winner: X” or “Winner: O”.
@@ -31,78 +30,69 @@ function Game() {
     const size = Math.sqrt(squares.length);
     // Check all rows
     for (let row = 0; row < size; row++) {
-        let count = 0;
-        let prev = null;
-        for(let col = 0; col < size; col++){
-            const current = squares[row * size + col];
-            if(current && current === prev){
-                count += 1;
-                if(count === k){
-                    return current;
-                }
-            } else {
-                count = 1;
-                prev = current;
-            }
+      let count = 0;
+      let prev = null;
+      for (let col = 0; col < size; col++) {
+        const current = squares[row * size + col];
+        if (current && current === prev) {
+          count += 1;
+          if (count === k) {
+            return current;
+          }
+        } else {
+          count = 1;
+          prev = current;
         }
+      }
     }
 
     // Check all columns
-    for(let col = 0; col < size; col++){
-        let count = 0;
-        let prev = null;
-        for (let row = 0; row < size; row++) {
-            const current = squares[row * size + col];
-            if(current && current === prev){
-                count += 1;
-                if(count === k){
-                    return current;
-                }
-            } else {
-                count = 1;
-                prev = current;
-            }
-        }
-    }
-    
-    // Check diagonals (top-left to bottom-right and top-right to bottom-left)
-    let count = 0;
-    let prev = null;
-    for (let i = 0; i < size; i++) {
-        const current = squares[i * size + i];
-        if(current && current === prev){
-            count += 1;
-            if(count === k){
-               return current;
-            }
+    for (let col = 0; col < size; col++) {
+      let count = 0;
+      let prev = null;
+      for (let row = 0; row < size; row++) {
+        const current = squares[row * size + col];
+        if (current && current === prev) {
+          count += 1;
+          if (count === k) {
+            return current;
+          }
         } else {
-           count = 1;
-           prev = current;
+          count = 1;
+          prev = current;
         }
+      }
     }
 
-    // Check counter diagonal
-    count = 0;
-    prev = null;
-    for (let i = 0; i < size; i++) {
-        const current = squares[i * size + (size - i - 1)];
-        if(current && current === prev){
-            count += 1;
-            if(count === k){
-                return current;
-            }
+    // Check diagonals
+    for (let i = 0; i < squares.length; i++) {
+      let count = 0;
+      let prev = null;
+      for (let j = i; j < squares.length; j += size + 1) {
+        const current = squares[j];
+        if (current && current === prev) {
+          count += 1;
+          if (count === k) {
+            return current;
+          }
         } else {
-            count = 1;
-            prev = current;
+          count = 1;
+          prev = current;
         }
+      }
     }
-};
 
 
-    //Declaring a Winner
-    useEffect(() => {
-      setWinner(calculateWinner(squares, k));
-    }, [squares, k]);
+  };
+
+  //Declaring a Winner
+  useEffect(() => {
+    setWinner(calculateWinner(squares, k));
+  }, [squares, k]);
+  useEffect(() => {
+    setSquares(Array(n ** 2).fill(null));
+    setSteps([Array(n ** 2).fill(null)]);
+  }, [n]);
   //Handle player
   const handleClick = (i) => {
     const squaresCopy = [...squares];
@@ -110,48 +100,55 @@ function Game() {
     squaresCopy[i] = xIsNext ? "X" : "O";
     setSquares(squaresCopy);
     setXIsNext(!xIsNext);
-    
-    const nonNullCount = squaresCopy.filter((square) => square!==null).length;
-    if (nonNullCount < steps.length){
-      setSteps(steps.slice(0, nonNullCount+1));
-    }
-    else{
+
+    const nonNullCount = squaresCopy.filter((square) => square !== null).length;
+    if (nonNullCount < steps.length) {
+      setSteps(steps.slice(0, nonNullCount + 1));
+    } else {
       setSteps([...steps, squaresCopy]);
     }
   };
 
   //Restart game
   const handlRestart = () => {
-    setSquares(Array(n**2).fill(null));
-    setSteps([Array(n**2).fill(null)]);
+    setSquares(Array(n ** 2).fill(null));
+    setSteps([Array(n ** 2).fill(null)]);
     setXIsNext(true);
   };
 
   return (
-    <div className = "main-container">
-    <div className="main">
-      <h2 className="result">Winner: {winner ? winner : "Not Yet"}</h2>
-      <div className = "play-option">
-        <div>Number of rows/columns: </div>
-        <input type="number" value = {n} onChange = {(e)=>setN(Number(e.target.value))}/>
+    <div className="main-container">
+      <div className="main">
+        <h2 className="result">Winner: {winner ? winner : "Not Yet"}</h2>
+        <div className="play-option">
+          <div>Number of rows/columns: </div>
+          <input
+            type="number"
+            value={n}
+            onChange={(e) => setN(Number(e.target.value))}
+          />
+        </div>
+        <div className="play-option">
+          <div>Number of squares in a row to win: </div>
+          <input
+            type="number"
+            value={k}
+            onChange={(e) => setK(Number(e.target.value))}
+          />
+        </div>
+
+        <div className="game">
+          <span className="player">Next player: {xIsNext ? "X" : "O"}</span>
+          <Board squares={squares} n={n} handleClick={handleClick} />
+        </div>
+        <button onClick={handlRestart} className="restart-btn">
+          Restart
+        </button>
       </div>
-      <div className = "play-option">
-        <div>Number of squares in a row to win: </div>
-        <input type="number" value = {k} onChange = {(e)=>setK(Number(e.target.value))}/>
+      <div className="history">
+        <h4>History</h4>
+        <History steps={steps} />
       </div>
-      
-      <div className="game">
-        <span className="player">Next player: {xIsNext ? "X" : "O"}</span>
-        <Board squares={squares} n = {n} handleClick={handleClick} />
-      </div>
-      <button onClick={handlRestart} className="restart-btn">
-        Restart
-      </button>
-    </div>
-    <div className="history"> 
-          <h4>History</h4>
-          <History steps={steps}/>
-    </div>
     </div>
   );
 }
